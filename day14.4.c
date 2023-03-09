1fd9a59f3bf013c4195a2e1500dad0cc235643cb
dhairya6570 <126230153+dhairya6570@users.noreply.github.com>
2023-03-09 09:18:38 -0500

    Add files via upload

---
+++ day14.4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main (){
+	
+	int n , sum=0 , firstdigit , lastdigit ;
+	
+	printf("Enter a value to find sum of first and last digit : ");
+    scanf("%d",&n);
+	
+	lastdigit = n%10 ;
+	
+	while(n >= 10){
+		
+		n=n/10;
+		
+	}
+	
+	firstdigit = n;
+	
+	sum = firstdigit + lastdigit ;
+	
+	printf("Sum of first and last digit : %d", sum);
+	
+	return 0 ;
+}
